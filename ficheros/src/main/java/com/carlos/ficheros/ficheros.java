0d6194cda24fb356c6798a1ed801495b0c52411b
Carlos <carlos.blasco.sogorb@gmail.com>
2022-09-23 15:31:21 +0200

    Primer commit del proyecto ficheros
    Una única función que es leerFichero()

---
+++ ficheros/src/main/java/com/carlos/ficheros/ficheros.java
@@ -0,0 +1,22 @@
+package com.carlos.ficheros;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ficheros {
+	public static List<String> leerFichero(String directorio, String fichero) {
+		try {
+			return Files.readAllLines((Path) Path.of(directorio+fichero), Charset.defaultCharset());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static List<String> leerFichero(String fichero){
+		return leerFichero("", fichero);
+	}
+}
