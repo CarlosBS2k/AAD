93390e1c1ab2a3cecf70d27d9331e9d7de2b6b0b
Carlos <carlos.blasco.sogorb@gmail.com>
2022-09-26 16:50:34 +0200

    Añadidas clase Persona y Serializacion

---
+++ serializacion/src/main/java/com/carlos/serializacion/entidades/Persona.java
@@ -0,0 +1,57 @@
+package com.carlos.serializacion.entidades;
+
+import java.util.Objects;
+
+public class Persona {
+	private String dni;
+	private String nombre;
+
+	public Persona() {
+		
+	}
+
+	public Persona(String dni, String nombre) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "dni=" + dni + ", nombre=" + nombre;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dni, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(dni, other.dni) && Objects.equals(nombre, other.nombre);
+	}
+	
+}
--- serializacion/src/main/java/com/carlos/serializacion/utils/serializacion.java
+++ serializacion/src/main/java/com/carlos/serializacion/utils/serializacion.java
@@ -1,5 +1,10 @@
 package com.carlos.serializacion.utils;
 
-public class serializacion {
+import com.carlos.serializacion.entidades.Persona;
 
+public class serializacion {
+	public boolean SerializarPersona(String directorio, String nombreFichero, Persona p) {
+		
+		return false;
+	}
 }
