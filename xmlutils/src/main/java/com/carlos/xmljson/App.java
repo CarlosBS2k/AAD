d371da457f750d17994ed9b22c46bdbcd89f7e83
Carlos <carlos.blasco.sogorb@gmail.com>
2022-09-30 18:40:28 +0200

    Creacion xmljson utils

---
+++ xmlutils/src/main/java/com/carlos/xmljson/App.java
@@ -0,0 +1,15 @@
+package com.carlos.xmljson;
+
+import com.carlos.xmljson.utilidades.XmlUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        XmlUtils.procesarXml();
+    }
+}
