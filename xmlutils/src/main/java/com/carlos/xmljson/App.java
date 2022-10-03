0276bcb332c055abcea8b80534749307eff71762
Carlos <carlos.blasco.sogorb@gmail.com>
2022-10-03 17:15:35 +0200

    fin XML con Sax, añadido XML con dom

--- xmlutils/src/main/java/com/carlos/xmljson/App.java
+++ xmlutils/src/main/java/com/carlos/xmljson/App.java
@@ -1,5 +1,7 @@
 package com.carlos.xmljson;
 
+import org.w3c.dom.ls.LSOutput;
+
 import com.carlos.xmljson.utilidades.XmlUtils;
 
 /**
@@ -10,6 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        XmlUtils.procesarXml();
+    	/*XmlUtils.procesarXmlSax("c:/ficheros", "asignaturas.xml").stream()
+    	.forEach(a->System.out.println(a));/**/
+    	//XmlUtils.mostrarXmlDom("C:/Ficheros/XML/asignaturas.xml");
+    	XmlUtils.procesarMarcaDom("https://e00-marca.uecdn.es/rss/portada.xml").forEach(a->System.out.println(a));
     }
 }
---
+++ xmlutils/src/main/java/com/carlos/xmlutils/entidades/Noticia.java
@@ -0,0 +1,45 @@
+package com.carlos.xmlutils.entidades;
+
+import java.util.Objects;
+
+public class Noticia {
+	String titular;
+	String descripcion;
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(descripcion, titular);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Noticia other = (Noticia) obj;
+		return Objects.equals(descripcion, other.descripcion) && Objects.equals(titular, other.titular);
+	}
+	public Noticia(String titular, String descripcion) {
+		super();
+		this.titular = titular;
+		this.descripcion = descripcion;
+	}
+	@Override
+	public String toString() {
+		return "Noticia [titular=" + titular + ", descripcion=" + descripcion + "]";
+	}
+	
+}
